326c5e3c1a44752ce42abaac9ac982ad171e20c9
digital <digital@owlishagency.com>
2022-10-27 17:07:09 -0400

    Add email and fix small issues

--- components/forms/layouts/SponsorshipFieldLayout.vue
+++ components/forms/layouts/SponsorshipFieldLayout.vue
@@ -43,7 +43,7 @@ export default {
             sponsorshipLevels: [
                 {
                     label: 'Star Tree Topper',
-                    value: '1.00',
+                    value: '10,000.00',
                     icon: '/sponsorship/sponsorship-tree-star-topper.svg',
                     invertedIcon:
                         '/sponsorship/sponsorship-tree-star-topper-inverted.svg',
